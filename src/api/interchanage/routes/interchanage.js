751f1edea214837c9228883294cf800c47dad2f5
njina <najina@gmail.com>
2022-06-20 19:46:43 +0900

    챗봇 콘텐트 타입 생성

---
+++ src/api/interchanage/routes/interchanage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * interchanage router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::interchanage.interchanage');
---
+++ src/api/interchanage/services/interchanage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * interchanage service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::interchanage.interchanage');
